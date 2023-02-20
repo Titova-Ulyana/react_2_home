61ef6d0edb3c2a2e300314f4f717e869e29fae7e
ulyana <adudududu02@mail.ru>
2023-02-21 00:11:12 +0300

    new file:   src/components/add.jsx 	new file:   src/components/authentication.jsx 	modified:   src/pages/add_pet.jsx 	new file:   src/pages/auth.jsx

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Footer from "../components/footer";
 import Add from "../components/add";
-import Header from '../components/header';
+import Header from "../components/header";
+import Header from './../components/header';
 
 function Add() {
   return (
     <div>
       <main style={{'minHeight':'70vh'}}>
-      <Header/>
-      <Add/>
+        <Header/>
+      <Authentication/>
       </main>
       <Footer/>
     </div>
